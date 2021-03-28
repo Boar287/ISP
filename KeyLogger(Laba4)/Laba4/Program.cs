f5c8894b8b881732aea7771ba36069876549d5db
Boar287 <79213467+Boar287@users.noreply.github.com>
2021-03-28 14:03:38 +0300

    Add files via upload

---
+++ KeyLogger(Laba4)/Laba4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using KeyLogger;
+
+namespace Laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KeyLog.Start();
+        }
+    }
+}
