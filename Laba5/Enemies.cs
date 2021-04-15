e299c07b6b0015d5e7720180d8656d196be00c3b
Boar287 <79213467+Boar287@users.noreply.github.com>
2021-04-15 08:21:56 +0300

    Add files via upload

---
+++ Laba5/Enemies.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba5
+{
+    abstract class Enemy
+    {
+        protected int _Damage;
+        protected double _Armor;
+        protected int _EvasionChance;
+        protected int _MissChance;
+        protected int _CritChance;
+        protected int _Health;
+        public string EnemyName { get; protected set; }
+        public bool EnabledAbility { get; protected set; }
+        public int AbilityCD { get; protected set; }
+        public int Stun { get; set; }
+        public void ReduceCD()
+        {
+            if (Stun != 0)
+                Stun--;
+            if (AbilityCD != 0)
+                AbilityCD--;
+
+        }
+
+        public int Damage { get { return _Damage; } }
+        public double Armor { get { return _Armor; } }
+        public int EvasionChance { get { return _EvasionChance; } }
+        public int MissChance { get { return _MissChance; } }
+        public int CritChance { get { return _CritChance; } }
+        public int Health
+        { 
+            get { return _Health; } 
+            set { _Health = value; }
+        }
+
+        public abstract void ShowInfo();
+        public abstract void UniqueEffect(Player player);
+
+        public void DealDamage(Player target)
+        {
+            Random rand = new Random();
+            int RandFactor = rand.Next(1, 15);
+            if (target.UsingShield == false)
+            {
+                if ((rand.Next(1, 100) <= MissChance) || (rand.Next(1, 100) <= target.EvadeChance))
+                {
+                    Console.WriteLine("Enemy MISS!");
+                    target.Health -= 0;
+                }
+                else
+                {
+                    if (rand.Next(1, 100) <= CritChance)
+                    {
+                        Console.WriteLine($"Enemy CRIT! -{Convert.ToInt32(((Damage + RandFactor) * 2) + ((Damage + RandFactor) * 2 * target.Armor / 1000))} ({Convert.ToInt32((Damage + RandFactor) * 2 * target.Armor / 1000)} Blocked)");
+                        target.Health -= Convert.ToInt32((((Damage + RandFactor) * 2) + ((Damage + RandFactor) * 2 * target.Armor / 1000)));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Enemy hit! -{Convert.ToInt32(Damage + RandFactor + target.Armor / 1000 * (Damage + RandFactor))} ({Convert.ToInt32(target.Armor / 1000 * (Damage + RandFactor))} Blocked)");
+                        target.Health -= Convert.ToInt32(((Damage + RandFactor) + (target.Armor / 1000 * (Damage + RandFactor))));
+                    }
+                }
+            }
+            else if(target.UsingShield==true)
+            {
+                if ((rand.Next(1, 100) <= MissChance) || (rand.Next(1, 100) <= target.EvadeChance))
+                {
+                    Console.WriteLine("Enemy MISS!");
+                    target.Health -= 0;
+                }
+                else if(rand.Next(1,100)<=10)
+                {
+                    Console.WriteLine("BLOCK!");
+                    target.Health -= 0;
+                }
+                else
+                {
+                    if (rand.Next(1, 100) <= CritChance)
+                    {
+                        Console.WriteLine($"Enemy CRIT! -{Convert.ToInt32(((Damage + RandFactor) * 2) + ((Damage + RandFactor) * 2 * target.Armor*2 / 1000))} ({Convert.ToInt32((Damage + RandFactor) * 2 * target.Armor*2 / 1000)} Blocked)");
+                        target.Health -= Convert.ToInt32((((Damage + RandFactor) * 2) + ((Damage + RandFactor) * 2 * target.Armor / 1000)));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Enemy hit! -{Convert.ToInt32(Damage + RandFactor + target.Armor*2 / 1000 * (Damage + RandFactor))} ({Convert.ToInt32(target.Armor*2 / 1000 * (Damage + RandFactor))} Blocked)");
+                        target.Health -= Convert.ToInt32(((Damage + RandFactor) + (target.Armor*2 / 1000 * (Damage + RandFactor))));
+                    }
+                }
+            }
+        }
+
+        public static void GetEnemiesInfo(DifficultyLevel difficultyLevel = DifficultyLevel.Easy)
+        {
+            List<Enemy> Enemies = new List<Enemy>() { new Orc(difficultyLevel), new Gnome(difficultyLevel), new Goblin(difficultyLevel) };
+            for(int i=0;i<Enemies.Count;i++)
+            {
+                Enemies[i].ShowInfo();
+            }
+        }
+        public Enemy(DifficultyLevel difficultyLevel)
+        {
+            if (difficultyLevel == DifficultyLevel.Hard)
+                EnabledAbility = true;
+            else
+                EnabledAbility = false;
+        }
+    }
+
+    class Gnome:Enemy
+    {
+        public Gnome(DifficultyLevel difficultyLevel=DifficultyLevel.Easy):base(difficultyLevel)
+        {
+            EnemyName = "Gnome";
+            _Damage = 33;
+            _Damage += Convert.ToInt32((_Damage * (int)difficultyLevel) / 100);
+            _Health = 300;
+            _Health += Convert.ToInt32((_Health * (int)difficultyLevel) / 100);
+            _EvasionChance = 20;
+            _CritChance = 25;
+            _Armor = 180;
+            _Armor += Convert.ToInt32((_Armor * (int)difficultyLevel) / 100);
+            _MissChance = 10;
+        }
+
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Gnome's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{_Damage}");
+            Console.WriteLine($"\tHealth:{_Health}");
+            Console.WriteLine($"\tEvasion Chance:{_EvasionChance}%");
+            Console.WriteLine($"\tCritical Chance:{_CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{_MissChance}%");
+            Console.WriteLine($"\tArmor:{_Armor}");
+            Console.WriteLine("Ability:");
+            Console.WriteLine("\tPassive<Dexterous hands>-takes one potion from your backpack (Once per battle)");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public override void UniqueEffect(Player player)
+        {
+            AbilityCD = 9999;
+            Console.WriteLine("Gnome steals a potion!");
+            Random rand=new Random();
+            player.Invenotry.RemoveAt(rand.Next(1, player.Invenotry.Count));
+
+        }
+    }
+
+    class Orc : Enemy
+    {
+        public Orc(DifficultyLevel difficultyLevel = DifficultyLevel.Easy) : base(difficultyLevel)
+        {
+            EnemyName = "Orc";
+            _Damage = 48;
+            _Damage += Convert.ToInt32((_Damage * (int)difficultyLevel) / 100);
+            _Health = 350;
+            _Health += Convert.ToInt32((_Health * (int)difficultyLevel) / 100);
+            _EvasionChance = 10;
+            _CritChance = 12;
+            _Armor = 350;
+            _Armor += Convert.ToInt32((_Armor * (int)difficultyLevel) / 100);
+            _MissChance = 27;
+        }
+
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Orc's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{_Damage}");
+            Console.WriteLine($"\tHealth:{_Health}");
+            Console.WriteLine($"\tEvasion Chance:{_EvasionChance}%");
+            Console.WriteLine($"\tCritical Chance:{_CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{_MissChance}%");
+            Console.WriteLine($"\tArmor:{_Armor}");
+            Console.WriteLine("Ability:");
+            Console.WriteLine("\tActive<Thunder tread>-stuns you for 1 tures (Once per 4 turnes)");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public override void UniqueEffect(Player player)
+        {
+            if (player.ClassName == "Warrior")
+            {
+                Random rand = new Random();
+                int StunChance = rand.Next(1, 2);
+                if (StunChance == 1)
+                {
+                    Console.WriteLine("Enemy stuns you");
+                    player.Stun = 2;
+                }
+                else
+                    Console.WriteLine("Enemy tried to stun you");
+
+            }
+            else
+            {
+                Console.WriteLine("Enemy stuns you!");
+                player.Stun = 2;
+            }
+            AbilityCD = 4;
+        }
+    }
+
+    class Goblin : Enemy
+    {
+        public Goblin(DifficultyLevel difficultyLevel = DifficultyLevel.Easy) : base(difficultyLevel)
+        {
+            EnemyName = "Goblin";
+            _Damage = 24;
+            _Damage += Convert.ToInt32((_Damage * (int)difficultyLevel) / 100);
+            _Health = 250;
+            _Health+= Convert.ToInt32((_Health * (int)difficultyLevel) / 100);
+            _EvasionChance = 27;
+            _CritChance = 30;
+            _Armor = 150;
+            _Armor+= Convert.ToInt32((_Armor * (int)difficultyLevel) / 100);
+            _MissChance = 5;
+
+        }
+
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Goblin's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{_Damage}");
+            Console.WriteLine($"\tHealth:{_Health}");
+            Console.WriteLine($"\tEvasion Chance:{_EvasionChance}%");
+            Console.WriteLine($"\tCritical Chance:{_CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{_MissChance}%");
+            Console.WriteLine($"\tArmor:{_Armor}");
+            Console.WriteLine("Ability:");
+            Console.WriteLine("\tPassive<Vigorous>-Has imune to stuns");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public override void UniqueEffect(Player player) { AbilityCD = 9999; DealDamage(player); }
+    }
+}
---
+++ Laba5/Weapons.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba5
+{
+    abstract class Weapon
+    {
+        protected int _Damage;
+        protected int _CritChance;
+        protected int _MissChance;
+        protected bool _OneHand;
+        public int RandFactor;
+        public string WeaponName { get; protected set; }
+
+        public int Damage { get { return _Damage; } }
+        public int CritChance { get { return _CritChance; } }
+        public int MissChance { get { return _MissChance; } }
+        public int BleedChance { get { return Damage; } }
+        public bool OneHand { get { return _OneHand; } }
+
+        public abstract void ShowInfo();
+
+        public static void GetWeaponsInfo()
+        {
+            List<Weapon> Weapon = new List<Weapon>() { new Dagger(), new Fist(), new TwoHandAxe(), new Staff(), new PoleArm() };
+            for(int i=0;i<Weapon.Count;i++)
+            {
+                Weapon[i].ShowInfo();
+            }
+        }
+    }
+
+    class Dagger : Weapon 
+    {
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Dagger's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{Damage}-{Damage+RandFactor}");
+            Console.WriteLine($"\tCritical chance:{CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{MissChance}%");
+            Console.WriteLine($"\tIn one hand:{OneHand}");
+            Console.WriteLine("Good choice for rogues");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public Dagger()
+        {
+            _Damage = 35;
+            _CritChance = 25;
+            _MissChance = 10;
+            RandFactor = 8;
+            _OneHand = true;
+            WeaponName = "Dagger";
+        }
+    }
+
+    class Fist : Weapon
+    {
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("First's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{Damage}-{Damage + RandFactor}");
+            Console.WriteLine($"\tCritical chance:{CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{MissChance}%");
+            Console.WriteLine($"\tChance to cause bleeding:{BleedChance}%");
+            Console.WriteLine($"\tIn one hand:{OneHand}");
+            Console.WriteLine("Good choice for rogues");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public Fist()
+        {
+            _Damage = 44;
+            _CritChance = 20;
+            _MissChance = 5;
+            RandFactor = 9;
+            _OneHand = true;
+            WeaponName = "Fist";
+        }
+    }
+
+    class TwoHandAxe : Weapon
+    {
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Two hand axe's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{Damage}-{Damage + RandFactor}");
+            Console.WriteLine($"\tCritical chance:{CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{MissChance}%");
+            Console.WriteLine($"\tChance to cause bleeding:{BleedChance}%");
+            Console.WriteLine($"\tIn one hand:{OneHand}");
+            Console.WriteLine("Not bad for warriors");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public TwoHandAxe()
+        {
+            _Damage = 66;
+            _CritChance = 15;
+            _MissChance = 20;
+            RandFactor = 15;
+            _OneHand = false;
+            WeaponName = "TwoHandAxe";
+        }
+    }
+
+    class Staff : Weapon
+    {
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Staff's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{Damage}-{Damage + RandFactor}");
+            Console.WriteLine($"\tCritical chance:{CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{MissChance}%");
+            Console.WriteLine($"\tChance to cause bleeding:{BleedChance}%");
+            Console.WriteLine($"\tIn one hand:{OneHand}");
+            Console.WriteLine("Mostly for mages");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public Staff()
+        {
+            _Damage = 71;
+            _CritChance = 5;
+            _MissChance = 8;
+            RandFactor = 16;
+            _OneHand = false;
+            WeaponName = "Staff";
+        }
+    }
+
+    class PoleArm : Weapon
+    {
+        public override void ShowInfo()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Polearm's's info:");
+            Console.ResetColor();
+            Console.WriteLine($"\tDamage:{Damage}-{Damage + RandFactor}");
+            Console.WriteLine($"\tCritical chance:{CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{MissChance}%");
+            Console.WriteLine($"\tChance to cause bleeding:{BleedChance}%");
+            Console.WriteLine($"\tIn one hand:{OneHand}");
+            Console.WriteLine("Not bad for warriors");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public PoleArm()
+        {
+            _Damage = 60;
+            _CritChance = 20;
+            _MissChance = 10;
+            RandFactor = 14;
+            _OneHand = false;
+            WeaponName = "PoleArm";
+        }
+    }
+
+    class Bow : Weapon
+    {
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Polearm's info:");
+            Console.WriteLine($"\tDamage:{Damage}-{Damage + RandFactor}");
+            Console.WriteLine($"\tCritical chance:{CritChance}%");
+            Console.WriteLine($"\tMiss Chance:{MissChance}%");
+            Console.WriteLine($"\tChance to cause bleeding:{BleedChance}%");
+            Console.WriteLine($"\tIn one hand:{OneHand}");
+            Console.WriteLine("\tYou have one extre turn in the beginnig of the fight");
+            Console.WriteLine("-------------------------------------------------");
+        }
+
+        public Bow()
+        {
+            _Damage = 65;
+            _CritChance = 15;
+            _MissChance = 15;
+            RandFactor = 15;
+            _OneHand = false;
+            WeaponName = "Bow";
+        }
+    }
+
+}
